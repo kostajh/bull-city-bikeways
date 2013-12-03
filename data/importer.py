d7fe961e5d54d4944feb0b12cbd1f2a6ea696114
Kosta Harlan <kostajh@gmail.com>
2013-12-02 20:36:54 -0500

    Update importer

--- data/importer.py
+++ data/importer.py
@@ -1,13 +1,15 @@
 #
 # Import route data from Google Docs and save as GeoJSON.
-
+#
 import gspread
 import json
 import os
 import configparser
+import hashlib
 
 
 def setUp():
+    """ Load the config """
     try:
         config_filename = os.path.join(
             os.path.dirname(__file__), "config")
@@ -21,10 +23,53 @@ def setUp():
               "You can create it from config.example in data/ directory."
         raise Exception(msg)
 
-# Get username/password from `config`
-email, password = setUp()
-# Login with your Google account
-gc = gspread.login(email, password)
 
-# Get submissions from Google Spreadsheets
-wks = gc.open_by_key("0AlZGBQh5vhE1dGFObVAtT0VKRjUxSUJjNnRaQlFIemc").sheet1
+def loadData():
+    """ Load data from the Google Spreadsheet, return a list of lists. """
+    # Get username/password from `config`
+    email, password = setUp()
+    # Login with your Google account
+    gc = gspread.login(email, password)
+
+    # Get submissions from Google Spreadsheets
+    wks = gc.open_by_key("0AlZGBQh5vhE1dGFObVAtT0VKRjUxSUJjNnRaQlFIemc").sheet1
+    return wks.get_all_values()[1:]
+
+
+def updateGeoJson(geojson, row):
+    """ Update the properties of the GeoJSON with Gdata values. """
+    data = geojson
+    # TODO.
+    return data
+
+
+def writeData(entries):
+    """ Write geojson into `data` dir and update dictionary.json """
+    for entry in entries:
+        if entry[0] is '':
+            continue
+        else:
+            date = entry[0].split(' ', 1)[0].replace("/", "-")
+            firstname = entry[1].split(' ', 1)[0].lower()
+            jsonhash = hashlib.sha256(entry[8].encode(
+                'utf-8')).hexdigest()[:16]
+            filename = "%s-%s-%s.geojson" % (date, firstname, jsonhash)
+            f = open(filename, 'w')
+            geojson = updateGeoJson(entry[8], entry)
+            f.write(geojson)
+            f.close()
+            # Update dictionary
+            f = open('dictionary.json', 'r')
+            data = json.loads(f.read())
+            f.close()
+            f = open('dictionary.json', 'w')
+            if filename not in data:
+                data.append(filename)
+            f.write(json.dumps(data))
+            f.close()
+    return
+
+# Load data.
+entries = loadData()
+# Write data.
+writeData(entries)
